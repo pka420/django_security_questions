33e048995d47707d6846380f3e97de219ec3d25c
Piyush <piyushkhurana38@gmail.com>
2024-09-11 17:38:54 +0530

    some fixes and bump the version

--- django_security_questions/models.py
+++ django_security_questions/models.py
@@ -13,8 +13,8 @@ class SecurityQuestion(models.Model):
     class Meta:
         db_table = "security_questions"
 
-    def __unicode__(self):
-        return _("%s") % self.question
+    def __str__(self):
+        return f"{self.question}"
 
 
 class SecurityAnswer(models.Model):
@@ -24,9 +24,33 @@ class SecurityAnswer(models.Model):
 
     class Meta:
         db_table = "security_answer"
+		unique_together = ('user', 'question')
 
-    def __unicode__(self):
-        return _("%s - %s") % (self.user, self.question)
+    def __str__(self):
+		return f"{self.user} - {self.question}"
+
+    def hash_current_answer(self):
+        self.set_answer(self.answer)
+
+    def set_answer(self, raw_answer):
+        if not bool(getattr(settings, "QUESTIONS_CASE_SENSITIVE", False)):
+            raw_answer = raw_answer.upper()
+        self.answer = hashers.make_password(raw_answer)
+
+    def check_answer(self, raw_answer):
+        if not bool(getattr(settings, "QUESTIONS_CASE_SENSITIVE", False)):
+            raw_answer = raw_answer.upper()
+
+        def setter(raw_answer):
+            self.set_answer(raw_answer)
+            self.save(update_fields=["answer"])
+        return hashers.check_password(raw_answer, self.answer, setter)
+
+    def set_unusable_answer(self):
+        self.answer = hashers.make_password(None)
+
+    def has_usable_answer(self):
+        return hashers.is_password_usable(self.answer)
 
     def hash_current_answer(self):
         self.set_answer(self.answer)
